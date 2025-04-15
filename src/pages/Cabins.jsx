178c0f21fb817b3831b253dff5b1ceea74b6e8b1
unknown <paulo.henriqq8@gmail.com>
2025-04-15 13:15:03 -0400

    setting up useQuery to fetch Cabin's data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="vertical">
-      <Heading as="h1">All cabins</Heading>
-      <img
-        src="https://dsvuczunvyyyofhskjww.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg"
-        alt=""
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>filter/sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
